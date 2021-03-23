2672335dd95a6a252b358f558149d8b3e83b019c
abdo.boutgardine <abdo.boutgadrine@gmail.com>
2021-03-23 16:25:47 +0100

    mise en place interface qualite/fournisseurs

--- resources/views/livewire/parametrage/fournisseurs.blade.php
+++ resources/views/livewire/parametrage/fournisseurs.blade.php
@@ -1,2 +1,100 @@
 @section('title', 'Fournisseurs')
 @section('header_title', 'Fournisseurs')
+
+<div class="d-flex flex-column-fluid">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Row-->
+        <div class="row">
+            <!--begin::Col-->
+            <div class="col-xl-12">
+                <!--begin::Card-->
+                <div class="card card-custom card-stretch gutter-b">
+                    <div class="card-header">
+                        <h3 class="card-title">{{ __('Liste Fournisseurs') }}</h3>
+                    </div>
+                    <div class="card-body">
+
+                        <!--begin::Flash message-->
+                        @if (session()->has('message'))
+                            <div class="alert alert-custom alert-light-success shadow fade show mb-5" role="alert">
+                                <div class="alert-icon"><i class="flaticon-interface-10"></i></div>
+                                <div class="alert-text">{{ session('message') }}</div>
+                                <div class="alert-close">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true"><i class="ki ki-close"></i></span>
+                                    </button>
+                                </div>
+                            </div>
+                        @endif
+                        <!--end::Flash message-->
+
+                        <!--Button trigger modal-->
+                        <button class="btn btn-primary font-weight-bold btn-pill" data-toggle="modal" data-target="#fournisseur">
+                            <i class="flaticon-plus"></i> {{ __('Ajouter Fournisseur') }}
+                        </button>
+                        <!--Modal-->
+                        <div wire:ignore.self class="modal fade" id="fournisseur" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="fournisseur" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">{{ __('Nouveau Fournisseur') }}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <i aria-hidden="true" class="ki ki-close"></i>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="fournisseur-form" class="form" wire:submit.prevent="createFournisseur">
+                                            <div class="form-group">
+                                                <div class="input-group input-group-prepend">
+                                                    <div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-user-tie icon-lg"></i></span></div>
+                                                    <input type="text" class="form-control" placeholder=" " wire:model.defer="nom"/>
+                                                    <label>{{ __('Nom') }}</label>
+                                                </div>
+                                                @error('nom')
+                                                    <span class="form-text text-danger">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-prepend">
+                                                    <div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-phone icon-lg"></i></span></div>
+                                                    <input type="tel" class="form-control" placeholder=" " wire:model.defer="phone"/>
+                                                    <label>{{ __('Phone') }}</label>
+                                                </div>
+                                                @error('phone')
+                                                    <span class="form-text text-danger">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="input-group input-group-prepend">
+                                                    <div class="input-group-prepend"><span class="input-group-text"><i class="fa fa-address-book icon-lg"></i></span></div>
+                                                    <input type="text" class="form-control" placeholder=" " wire:model.defer="contact"/>
+                                                    <label>{{ __('Contact') }}</label>
+                                                </div>
+                                                @error('contact')
+                                                    <span class="form-text text-danger">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal">{{ __('Fermer') }}</button>
+                                        <button type="submit" class="btn btn-primary font-weight-bold" form="fournisseur-form">{{ __('Enregistrer') }}</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Table-->
+                        <div class="mt-5">
+                            {{-- @livewire('parametrage....') --}}
+                        </div>
+                    </div>
+                </div>
+                <!--end::Card-->
+            </div>
+            <!--end::Col-->
+        </div>
+        <!--end::Row-->
+    </div>
+    <!--end::Container-->
+</div>
