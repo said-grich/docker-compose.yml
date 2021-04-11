438a97ada7e05430285d436c5ad928faa6a78a31
farah_elalem <farah.el1996@gmail.com>
2021-04-11 10:13:20 +0000

    wip livraison

---
+++ app/Http/Livewire/Vente/ListeCommandeValidee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Vente;
+
+use Livewire\Component;
+
+class ListeCommandeValidee extends Component
+{
+    public function render()
+    {
+        return view('livewire.vente.liste-commande-validee');
+    }
+}
--- resources/views/livewire/commandes.blade.php
+++ resources/views/livewire/commandes.blade.php
@@ -18,8 +18,55 @@
                         @include('layouts.partials.alerts')
                         <!--end::Alerts-->
 
+                        <div class="mt-5">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" data-toggle="tab" href="#recues-tab">
+                                        <span class="nav-text">Reçues</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-toggle="tab" href="#validees-tab">
+                                        <span class="nav-text">Validées</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-toggle="tab" href="#pretes-tab">
+                                        <span class="nav-text">Prêtes</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-toggle="tab" href="#en-expedition-tab">
+                                        <span class="nav-text">En Expédition</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" data-toggle="tab" href="#livree-tab">
+                                        <span class="nav-text">Livrée</span>
+                                    </a>
+                                </li>
+                            </ul>
+                            <div class="tab-content mt-5">
+                                <div class="tab-pane fade active show" id="recues-tab" role="tabpanel">
+                                    @livewire('vente.liste-commandes')
+                                </div>
+                                <div class="tab-pane fade" id="validees-tab" role="tabpanel">
+                                    @livewire('vente.liste-commandes')
+                                </div>
+                                <div class="tab-pane fade" id="pretes-tab" role="tabpanel">
+                                    @livewire('vente.liste-commandes')
+                                </div>
+                                <div class="tab-pane fade" id="en-expedition-tab" role="tabpanel">
+                                    @livewire('vente.liste-commandes')
+                                </div>
+                                <div class="tab-pane fade" id="livrees-tab" role="tabpanel">
+                                    @livewire('vente.liste-commandes')
+                                </div>
+                            </div>
+                        </div>
+
                         <!--Table-->
-                        @livewire('vente.liste-commandes')
+                        {{-- @livewire('vente.liste-commandes') --}}
                     </div>
                 </div>
                 <!--end::Card-->
