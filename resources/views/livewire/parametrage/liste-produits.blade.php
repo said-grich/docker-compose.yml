701072b0ca7e963c87766694a6139b694491840e
Nouhaila <bendarqaouino@gmail.com>
2021-04-15 16:49:32 +0100

    wip

--- resources/views/livewire/parametrage/liste-produits.blade.php
+++ resources/views/livewire/parametrage/liste-produits.blade.php
@@ -194,7 +194,7 @@
         <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">{{ __('Modification unité') }}</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">{{ __('Modification produit') }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <i aria-hidden="true" class="ki ki-close"></i>
                     </button>
@@ -297,7 +297,7 @@
                                     @endforeach
                                 </select>
                                 <div class="input-group-append" data-toggle="modal"
-                                     data-target="#tranches">
+                                     data-target="#tranchese">
                                     <button class="btn btn-primary" type="button"
                                             data-toggle="tooltip" data-theme="dark"
                                             title="Ajouter Tranche"><i
@@ -407,6 +407,84 @@
                     </div>
                 </div>
 
+            </div>
+        </div>
+    </div>
+    <!--Modal-Tranches-->
+
+    <div class="modal secondary fade" id="tranchese" data-backdrop="static" tabindex="-1"
+    role="dialog" aria-labelledby="tranches" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ __('Nouvelle tranche') }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i aria-hidden="true" class="ki ki-close"></i>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="tranchess-form" class="form" wire:submit.prevent="createTranches">
+                        <div x-data="{ open: false }">
+                            <div x-data="{'showPoids': @entangle('showPoids')}">
+                                <div class="row" x-show="showPoids">
+
+                                    <div class="form-group col-md-6">
+                                        <div class="input-group input-group-prepend">
+                                            <div class="input-group-prepend"><span
+                                                    class="input-group-text"><i
+                                                        class="fa fa-sliders-h icon-lg"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" placeholder=" "
+                                                    wire:model.defer="minPoids"/>
+                                            <label>{{ __('Poids Minimal') }}</label>
+                                        </div>
+                                        @error('minPoids')
+                                        <span class="form-text text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <div class="input-group input-group-prepend">
+                                            <div class="input-group-prepend"><span
+                                                    class="input-group-text"><i
+                                                        class="fa fa-sliders-h icon-lg"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" placeholder=" "
+                                                    wire:model.defer="maxPoids"/>
+                                            <label>{{ __('Poids Maximal') }}</label>
+                                        </div>
+                                        @error('maxPoids')
+                                        <span class="form-text text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div x-data="{'showKgPiece': @entangle('showKgPiece')}">
+                                <div x-show="showKgPiece">
+                                    <div class="form-group">
+                                        <div class="input-group input-group-prepend">
+                                            <div class="input-group-prepend"><span
+                                                    class="input-group-text"><i
+                                                        class="fa fa-weight-hanging icon-lg"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control" placeholder=" "
+                                                wire:model.defer="nomTranche"/>
+                                            <label>{{ __('Kg/Pièce') }}</label>
+                                        </div>
+                                        @error('nomTranche')
+                                        <span class="form-text text-danger">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-primary font-weight-bold"
+                            data-dismiss="modal">{{ __('Fermer') }}</button>
+                    <button type="submit" class="btn btn-primary font-weight-bold"
+                            form="tranchess-form">{{ __('Enregistrer') }}</button>
+                </div>
             </div>
         </div>
     </div>
