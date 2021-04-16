ee8f3ba044df302a732ef0c9ebea0818dfdf6acc
chaimaejeb <chaimaejeb87@gmail.com>
2021-04-16 14:19:27 +0000

    wip

--- resources/views/livewire/parametrage/liste-categorie.blade.php
+++ resources/views/livewire/parametrage/liste-categorie.blade.php
@@ -99,7 +99,7 @@
                                 <span class="form-text text-danger">{{ $message }}</span>
                             @enderror
                         </div>
-                        <div class="form-group ">
+                        <div class="form-groupcol ">
                             <label class="col-3 col-form-label">Active</label>
                             <div class="col-3">
                                 <span class="switch switch-outline switch-icon switch-primary">
